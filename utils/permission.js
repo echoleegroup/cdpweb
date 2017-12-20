6d8e9ac527d6f05179b986eef93968826fc68d33
Andrew <andrew.hl.huang@maxnerva.com>
2017-12-20 16:10:22 +0800

    增加權限對應Menucode檔案 1.menucode對應資料表sy_menu.menuCode

---
+++ utils/permission.js
@@ -0,0 +1,22 @@
+"use strict";
+var MENU_CODE={
+  USER: "_mgrUser",
+  USER_ROLE: "_mgrRole",
+  CUST_GENE: "_modelChar",
+  CUST_MOTIVATION: "_modelPred",
+  CUST_OBRV: "_modelBhv",
+  EVENT_PAGE_LIST: "_actad_actList",
+  EVENT_PAGE_EDIT: "_actad_actEdit",
+  FEED_DATA_LIST: "_feedDataList",
+  FEED_DATA_OUT: "_feedDataOutEdit",
+  FEED_DATA_NCBS: "_feedDataNCBSEdit",
+  GENE_RAUDIC: "_targetCal",
+  MODEL_DOWNLOAD: "_targetDao",
+  MODEL_LIST: "_modelOvv",
+  TA_DISPATCH_UPLOAD: "_taDispach",
+  TA_REACTION_UPLOAD: "_taReaction"
+};
+
+exports.menucode = MENU_CODE;
+
+//# sourceMappingURL=config.js.map
