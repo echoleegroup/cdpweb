7d054ef4693d613424a60c75654294dc723506f1
Andrew <andrew.hl.huang@maxnerva.com>
2017-12-21 15:29:29 +0800

    1)增加下載服務(download-service.js) 2)外部清單增加middleware判斷登入與權限 3)清單下載改為呼叫WEBSERVICE

---
+++ services/download-service.js
@@ -0,0 +1,21 @@
+'user strict'
+
+const Q = require('q');
+const mssql = require('mssql');
+const winston = require('winston');
+const _connector = require('../utils/sql-query-util');
+
+module.exports.getFilePath = (listid, datasource, callback = () => { }) => {
+  let sql = 'SELECT origName,uniqName ';
+  if (datasource == 'NCBS')
+    sql += 'FROM cu_NCBSMst WHERE ncbsID = ' + listid;
+  else if (datasource == 'outdata')
+    sql += 'FROM cu_OuterListMst WHERE outerListID =' + listid;
+  winston.info(sql);
+  Q.nfcall(_connector.execSqlByParams, sql, {}).then((resultSet) => {
+    callback(null, resultSet[0]);
+  }).fail(err => {
+    winston.error('====[download] get  downloadvalue failed: ', err);
+    callback(err);
+  });
+};
