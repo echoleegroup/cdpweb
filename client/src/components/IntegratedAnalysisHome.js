2496304f5152a51e1b182bbf8dd9628b06973a6f
HD <>
2018-01-26 20:36:40 +0800

    refacroty check finsh edit before step forward

--- client/src/components/IntegratedAnalysisHome.js
+++ client/src/components/IntegratedAnalysisHome.js
@@ -9,14 +9,47 @@ import IntegratedAnalysisCriteriaTrail from "./IntegratedAnalysisCriteriaTrail";
 import IntegratedAnalysisCriteriaPreview from "./IntegratedAnalysisCriteriaPreview";
 import IntegratedAnalysisFeaturePicker from "./IntegratedAnalysisOutputFeaturePicker";
 
-const STEPS = Object.freeze({
+const criteriaStepForwardHandler = (targetStep, _that) => {
+  console.log('criteriaStepForwardHandler: ', _that.state.step);
+  let isReady = _that.stepComponent.isReadyToLeave();
+  console.log('IntegratedAnalysisHome::isReady: ', isReady);
+
+  if (isReady) {
+    let criteria = _that.stepComponent.getCriteria();
+    console.log('IntegratedAnalysisHome::criteria: ' , criteria);
+    _that.setState(prevState => {
+      return {
+        criteria: prevState.criteria.set(prevState.step, criteria)
+      };
+    });
+    _that.setStep(targetStep);
+  } else {
+    window.alert('Please confirm your criteria before leaving.');
+  }
+};
+
+const stepForwardHandler = (targetStep, _that) => {
+  _that.setStep(targetStep);
+};
+
+const STEPS = {
   step1: 'client',
   step2: 'vehicle',
   step3: 'transaction',
   step4: 'tag',
   step5: 'trail',
-  step6: 'preview',
-  step7: 'feature'
+  step6: '_preview',
+  step7: '_features'
+};
+
+const STEP_FORWARD_HANDLERS = Object.freeze({
+  [STEPS.step1]: criteriaStepForwardHandler,
+  [STEPS.step2]: criteriaStepForwardHandler,
+  [STEPS.step3]: criteriaStepForwardHandler,
+  [STEPS.step4]: criteriaStepForwardHandler,
+  [STEPS.step5]: criteriaStepForwardHandler,
+  [STEPS.step6]: stepForwardHandler,
+  [STEPS.step7]: stepForwardHandler,
 });
 
 export default class IntegratedAnalysisHome extends BodyLayout {
@@ -32,22 +65,11 @@ export default class IntegratedAnalysisHome extends BodyLayout {
         [STEPS.step5]: []
       })
     };
-    // this.state = {
-    //   client_profile: {
-    //     criteria: []
-    //   },
-    //   vehicle: {
-    //     criteria:[]
-    //   },
-    //   transaction: {
-    //     criteria: []
-    //   }
-    // };
   };
 
   componentWillMount() {
 
-    this.stepTo = (targetStep) => {
+    this.setStep = (targetStep) => {
       console.log('IntegratedAnalysisHome::stepTo: ' , targetStep);
 
       this.setState({
@@ -55,29 +77,14 @@ export default class IntegratedAnalysisHome extends BodyLayout {
       });
     };
 
-    this.stepToCriteriaHandler = (targetStep) => {
+    this.stepTo = (targetStep) => {
       return () => {
-        let isReady = this.stepComponent.isReadyToLeave();
-        console.log('IntegratedAnalysisHome::isReady: ', isReady);
-
-        if (isReady) {
-          let criteria = this.stepComponent.getCriteria();
-          console.log('IntegratedAnalysisHome::criteria: ' , criteria);
-          this.setState(prevState => {
-            return {
-              criteria: prevState.criteria.set(prevState.step, criteria)
-            };
-          });
-          this.stepTo(targetStep);
-        } else {
-          window.alert('Please confirm your criteria before leaving.');
-        }
+        this.setStep(targetStep);
       };
     };
 
-    this.stepToHandler = (targetStep) => {
-      return () => {
-        this.stepTo(targetStep);
+    this.stepToHandler = () => {
+      return (targetStep) => {
       };
     }
 
@@ -93,54 +100,53 @@ export default class IntegratedAnalysisHome extends BodyLayout {
                                                  criteria={this.state.criteria.get(STEPS.step1)}
                                                  params={this.params}
                                                  step={STEPS.step1}
-                                                 stepNext={this.stepToCriteriaHandler(STEPS.step2)}/>;
+                                                 stepNext={this.stepTo(STEPS.step2)}/>;
       case STEPS.step2:
         return <IntegratedAnalysisCriteriaVehicle ref={this.storeCurrentStepComponent}
                                                   criteria={this.state.criteria.get(STEPS.step2)}
                                                   params={this.params}
                                                   step={STEPS.step2}
-                                                  stepPrev={this.stepToCriteriaHandler(STEPS.step1)}
-                                                  stepNext={this.stepToCriteriaHandler(STEPS.step3)}/>;
+                                                  stepPrev={this.stepTo(STEPS.step1)}
+                                                  stepNext={this.stepTo(STEPS.step3)}/>;
       case STEPS.step3:
         return <IntegratedAnalysisCriteriaTransaction ref={this.storeCurrentStepComponent}
                                                       criteria={this.state.criteria.get(STEPS.step3)}
                                                       params={this.params}
                                                       step={STEPS.step3}
-                                                      stepPrev={this.stepToCriteriaHandler(STEPS.step2)}
-                                                      stepNext={this.stepToCriteriaHandler(STEPS.step4)}/>;
+                                                      stepPrev={this.stepTo(STEPS.step2)}
+                                                      stepNext={this.stepTo(STEPS.step4)}/>;
       case STEPS.step4:
         return <IntegratedAnalysisCriteriaTag ref={this.storeCurrentStepComponent}
                                               criteria={this.state.criteria.get(STEPS.step4)}
                                               params={this.params}
                                               step={STEPS.step4}
-                                              stepPrev={this.stepToCriteriaHandler(STEPS.step3)}
-                                              stepNext={this.stepToCriteriaHandler(STEPS.step5)}/>;
+                                              stepPrev={this.stepTo(STEPS.step3)}
+                                              stepNext={this.stepTo(STEPS.step5)}/>;
       case STEPS.step5:
         return <IntegratedAnalysisCriteriaTrail ref={this.storeCurrentStepComponent}
                                                 criteria={this.state.criteria.get(STEPS.step5)}
                                                 params={this.params}
                                                 step={STEPS.step5}
-                                                stepPrev={this.stepToCriteriaHandler(STEPS.step4)}
-                                                stepNext={this.stepToCriteriaHandler(STEPS.step6)}/>;
+                                                stepPrev={this.stepTo(STEPS.step4)}
+                                                stepNext={this.stepTo(STEPS.step6)}/>;
       case STEPS.step6:
         return <IntegratedAnalysisCriteriaPreview ref={this.storeCurrentStepComponent}
                                                   criteria={this.state.criteria}
                                                   params={this.params}
                                                   step={STEPS.step6}
                                                   STEPS={STEPS}
-                                                  stepPrev={this.stepToHandler(STEPS.step5)}
-                                                  stepNext={this.stepToHandler(STEPS.step7)}/>;
+                                                  //stepPrev={this.stepTo(STEPS.step5)}
+                                                  stepNext={this.stepTo(STEPS.step7)}/>;
       case STEPS.step7:
         return <IntegratedAnalysisFeaturePicker ref={this.storeCurrentStepComponent}
                                                 params={this.params}
-                                                step={STEPS.step7}
-                                                stepPrev={this.stepToHandler(STEPS.step6)}/>;
+                                                step={STEPS.step7}/>;
 
     }
   };
 
   ComponentSideBar() {
-    return <IntegratedAnalysisSideBar stepTo={this.stepTo}/>
+    return <IntegratedAnalysisSideBar stepTo={this.stepToHandler()}/>
   };
 };
 
