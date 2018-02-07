a16f968ef7383e780cf64788b4dabbd2225431bc
HD <hd.wh.cheng@maxnerva.com>
2018-02-07 10:25:04 +0800

    1.) alter table ft_Feature, change varchar to nvarchar 2.) drop cd_Feature and rebuild

---
+++ migrations/2018020702_recreate_table_cd_Feature.js
@@ -0,0 +1,74 @@
+'use strict';
+
+// const winston = require('winston');
+
+module.exports = {
+  up: (query, DataTypes) => {
+    return query.dropTable('cd_Feature').then(res => {
+      return query.createTable('cd_Feature', {
+        featID: {
+          type: DataTypes.STRING(50),
+          primaryKey: true,
+          allowNull: false
+        },
+        featName: {
+          type: DataTypes.STRING(100)
+        },
+        featNameAbbr: {
+          type: DataTypes.STRING(100)
+        },
+        featNameExt: {
+          type: DataTypes.STRING(50)
+        },
+        featCateg: {
+          type: DataTypes.STRING(3)
+        },
+        dataType: {
+          type: DataTypes.STRING(20)
+        },
+        dataSize: {
+          type: DataTypes.INTEGER
+        },
+        chartType: {
+          type: DataTypes.STRING(20)
+        },
+        minPeriod: {
+          type: DataTypes.STRING(3)
+        },
+        dataSource: {
+          type: DataTypes.STRING(10)
+        },
+        isCode: {
+          type: DataTypes.STRING(1)
+        },
+        codeGroup: {
+          type: DataTypes.STRING(20)
+        },
+        featDesc: {
+          type: DataTypes.STRING(200)
+        },
+        featRemark: {
+          type: DataTypes.STRING(200)
+        },
+        featClient: {
+          type: DataTypes.STRING(20)
+        },
+        isDel: {
+          type: DataTypes.STRING(1)
+        },
+        crtTime: {
+          type: DataTypes.DATE
+        },
+        updTime: {
+          type: DataTypes.DATE
+        },
+        updUser: {
+          type: DataTypes.STRING(20)
+        },
+        condiPickType: {
+          type: DataTypes.STRING(20)
+        }
+      });
+    });
+  }
+};
