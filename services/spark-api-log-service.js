e2b12e82ce73ef8b9212c542be7f99b77b5b2f42
Andrew <andrew.hl.huang@maxnerva.com>
2018-03-19 16:20:56 +0800

    增加非車主查詢api

---
+++ services/spark-api-log-service.js
@@ -0,0 +1,28 @@
+'use strict';
+const winston = require('winston');
+const API_360_HOST = require("../app-config").get("API_360_HOST");
+const API_360_PORT = require("../app-config").get("API_360_PORT");
+module.exports.transService = (queryId, JObject, callback) => {
+  //呼叫API
+  
+  let request = require('request');
+  let url = "http://" + API_360_HOST + ":" + API_360_PORT + "/query_nonowner/" + queryId
+  request({
+    url: url,
+    method: "POST",
+    json: true,
+    headers: {
+      "content-type": "application/json",
+    },
+    body: JSON.stringify(JObject)
+  }, function (error, response, body) {
+    if (error)
+      callback(error, null);
+    else
+      callback(null, JObject);
+  });
+
+
+
+
+};
