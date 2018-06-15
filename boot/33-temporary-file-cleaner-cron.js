c26f9ffde23153fcdfed752cf6dfc2a5935c290c
HD <hd.wh.cheng@maxnerva.com>
2018-06-15 17:03:22 +0800

    remove temporay file produced by multer module.

---
+++ boot/33-temporary-file-cleaner-cron.js
@@ -0,0 +1,30 @@
+const _ = require('lodash');
+const fs = require('fs');
+const path = require('path');
+const Q = require('q');
+const schedule = require('node-schedule');
+const winston = require('winston');
+const moment = require('moment');
+const constants = require('../utils/constants');
+const _connector = require('../utils/sql-query-util');
+
+module.exports = (app) => {
+  let dailyTaskReportCron = {
+    minute: 33,
+    hour: 1
+  };
+
+  // schedule.scheduleJob(`0 * * * * *`, () => {
+  schedule.scheduleJob(`${dailyTaskReportCron.minute} ${dailyTaskReportCron.hour} * * *`, () => {
+    const today = moment().startOf('day').valueOf();
+
+    fs.readdirSync(constants.ASSERTS_CUSTOM_TARGET_ASSERTS_PATH_ABSOLUTE).forEach(file => {
+      console.log(file);
+      let filePath = path.resolve(constants.ASSERTS_CUSTOM_TARGET_ASSERTS_PATH_ABSOLUTE, file);
+      let stat = fs.statSync(filePath);
+      if (!stat.isDirectory() && file.indexOf('.') < 0 && stat.birthtimeMs < today) {
+        fs.unlinkSync(filePath);
+      }
+    });
+  });
+};
