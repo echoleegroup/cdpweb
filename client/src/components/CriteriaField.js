e2cc9415ce7d7aad7dc71603463f1828dbf13ab0
HD <hd.wh.cheng@maxnerva.com>
2019-04-11 16:48:34 +0800

    bug fix

--- client/src/components/CriteriaField.js
+++ client/src/components/CriteriaField.js
@@ -71,7 +71,7 @@ export default class CriteriaField extends React.PureComponent {
   };
 
   displayTextFormatter(inputType, value, valueLabel) {
-    if (!!value)
+    if (!value)
       return null;
 
     switch (inputType) {
